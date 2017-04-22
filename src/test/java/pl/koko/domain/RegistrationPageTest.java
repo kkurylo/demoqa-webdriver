ab27759aae8ce3b896583e786214252265c53cb8
Kamila Kuryło <kamila888k@gmail.com>
2017-04-23 01:02:13 +0200

    Basic implementation

---
+++ src/test/java/pl/koko/domain/RegistrationPageTest.java
@@ -0,0 +1,39 @@
+package pl.koko.domain;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RegistrationPageTest {
+
+    WebDriver driver = new ChromeDriver();
+    RegistrationPage registrationPage = new RegistrationPage(driver);
+
+    @BeforeMethod
+    public void setUp() {
+        registrationPage.openPage();
+    }
+
+    @Test
+    public void shouldRegisterNewUser() {
+        registrationPage.setFirstNameField("Napoleon");
+        registrationPage.setLastNameField("Bonaparte");
+        registrationPage.selectCountryField("Brazil");
+        registrationPage.selectMonthField("4");
+        registrationPage.selectDayField("14");
+        registrationPage.selectYearField("2012");
+        registrationPage.setPhoneField("4834567890");
+        registrationPage.setUsernameField("napoleonBonaparte4567");
+        registrationPage.setEmailField("napoleonBonaparte@gmail.com");
+        registrationPage.setDescriptionField("Being a lady");
+        registrationPage.setPasswordField("Password100pas");
+        registrationPage.setConfirmPasswordField("Password100pas");
+    }
+
+    @AfterTest
+    public void chillOut() {
+        driver.close();
+    }
+}
